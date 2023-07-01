9c57031af03c864ed57264a218a6e2d8084e6183
Ravinder Singh <ravinder_m210659ca@nitc.ac.in>
2023-07-01 12:56:05 +0530

    More Improvement in Category Slices

--- client/src/redux/slices/category/categorySlice.js
+++ client/src/redux/slices/category/categorySlice.js
@@ -7,6 +7,38 @@ const resetEditAction = createAction("category/reset");
 const resetDeleteAction = createAction("category/delete-reset");
 const resetCategoryAction = createAction("category/created-reset");
 
+//action
+export const createCategoryAction = createAsyncThunk(
+  "category/create",
+  async (category, { rejectWithValue, getState, dispatch }) => {
+    //get user token
+    const user = getState()?.users;
+    const { userAuth } = user;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userAuth?.token}`,
+      },
+    };
+    //http call
+    try {
+      const { data } = await axios.post(
+        `${baseUrl}/api/category`,
+        {
+          title: category?.title,
+        },
+        config
+      );
+      //disoatch action
+      dispatch(resetCategoryAction());
+      return data;
+    } catch (error) {
+      if (!error?.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
 
 //fetch all action
 export const fetchCategoriesAction = createAsyncThunk(
