beb403d1a8a6f78335a059970b9ae41023420492
Ravinder Singh <ravinder_m210659ca@nitc.ac.in>
2023-07-03 18:00:56 +0530

    Added User functionality in  Post Model

--- server/model/post/Post.js
+++ server/model/post/Post.js
@@ -37,7 +37,20 @@ const postSchema = new mongoose.Schema(
         ref: "User",
       },
     ],
-    
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please Author is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Post description is required"],
+    },
+    image: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2020/10/25/09/23/seagull-5683637_960_720.jpg",
+    },
   },
   {
     toJSON: {
