d781d35bfc9bfc46ed26aad7344e1b8703a2e0b9
Ravinder Singh <ravinder_m210659ca@nitc.ac.in>
2023-07-12 01:50:49 +0530

    Added Formik in Comment's Component

--- client/src/components/Comments/AddComment.js
+++ client/src/components/Comments/AddComment.js
@@ -16,7 +16,15 @@ const AddComment = ({ postId }) => {
   const comment = useSelector(state => state?.comment);
   const { loading, appErr, serverErr } = comment;
 
-  
+  const formik = useFormik({
+    initialValues: {
+      description: "",
+    },
+    onSubmit: values => {
+      const data = {
+        postId,
+        description: values?.description,
+      };
       //dispatch action
       dispatch(createCommentAction(data));
     },
