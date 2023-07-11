50d96a6cc9684b3668979fd7238367c3923159e3
Ravinder Singh <ravinder_m210659ca@nitc.ac.in>
2023-07-12 01:56:47 +0530

    Solved Error of photoUpload

--- server/middlewares/uploads/photoUpload.js
+++ server/middlewares/uploads/photoUpload.js
@@ -4,22 +4,6 @@ const path = require("path");
 //storage
 const multerStorage = multer.memoryStorage();
 
-//file type checking
-const multerFilter = (req, file, cb) => {
-  //check file type
-  if (file.mimetype.startsWith("image")) {
-    cb(null, true);
-  } else {
-    //rejected files
-    cb(
-      {
-        message: "Unsupported file format",
-      },
-      false
-    );
-  }
-};
-
 const photoUpload = multer({
   storage: multerStorage,
   fileFilter: multerFilter,
